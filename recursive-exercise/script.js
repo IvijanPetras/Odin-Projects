1af3a2babd1fd22a05f115a886015bfe50fc1079
Ivijan <iapetras@hotmail.com>
2023-03-15 22:58:26 +0100

    mergeSort

--- recursive-exercise/script.js
+++ recursive-exercise/script.js
@@ -80,5 +80,29 @@ window.onload = () => {
     return arr;
   }
 
-    console.log(fibonacciRecursive(13));
+    //console.log(fibonacciRecursive(13));
+
+    function merge(left,right){
+        let sortedArr = []
+
+        while(left.length && right.length){
+            if(left[0] < right[0]) {
+                sortedArr.push(left.shift())
+            } else  {
+                sortedArr.push(right.shift())
+            }
+        }
+
+        return [...sortedArr, ...left, ...right]
+    }
+
+    function mergeSort(arr) {
+        if (arr.length <= 1) return arr
+        let mid = Math.floor(arr.length / 2)
+        let left = mergeSort(arr.slice(0, mid))
+        let right = mergeSort(arr.slice(mid))
+        return merge(left, right)
+      }
+
+      console.log(mergeSort([2,5,67,45,3,48]))
 };
