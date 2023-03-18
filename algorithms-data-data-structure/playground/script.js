43479c53a306585130ff153c555ec7056b75f41a
Ivijan <iapetras@hotmail.com>
2023-03-18 21:07:54 +0100

    palindrome faster solution

--- algorithms-data-data-structure/playground/script.js
+++ algorithms-data-data-structure/playground/script.js
@@ -33,26 +33,18 @@ window.onload = () => {
   // console.log(twoSum2([1, 3, 10, 11, 14], 13))
 
   var isPalindrome = function (x) {
-    let isValid = true;
-    let arr = String(x).split("").map((num)=>{
-        return Number(num)
-      })
-    console.log(arr.length)
-    if(isNaN(arr[0])) return false
-    if(arr.length === 2 && arr[0] !== arr[1]) return false
-    function isEven() {
-      if (arr.length % 2 === 0) return arr.length / 2
-       return Math.round(arr.length / 2) + 1
+    if (x < 0) return false
+    let xString = x.toString()
+    let f = xString.slice(0, xString.length/2)
+    let s = ''
+    let i = 1
+    while(s.length < f.length){
+        s += xString[xString.length - i]
+        i++
     }
-    
-    console.log(isEven())
-    for (let i = 0; i < isEven(); i++) {
-        if (x[i] !== x[x.length - 1 - i]) {
-            isValid = false
-        } 
-    }
-    return isValid
+    if (f == s) return true
+    if (f !== s) return false
   };
 
-  console.log(isPalindrome(100));
+  console.log(isPalindrome(1001))
 };
