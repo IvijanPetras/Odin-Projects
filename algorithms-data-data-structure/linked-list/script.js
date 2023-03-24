ab2e188415ee8f96ce69bbd40db74aee05e7ad43
Ivijan <iapetras@hotmail.com>
2023-03-24 20:24:31 +0100

    few more methods done

--- algorithms-data-data-structure/linked-list/script.js
+++ algorithms-data-data-structure/linked-list/script.js
@@ -6,13 +6,13 @@ class Node {
 }
 
 class LinkedList {
-  constructor() {
+  constructor(){
     this.head = null;
     this.size = 0;
   }
 
   // adds value at end
-  append(data) {
+  append(data){
     const node = new Node(data);
     let current;
 
@@ -29,24 +29,70 @@ class LinkedList {
     this.size++;
   }
   // adds value at start 
-  prepend(data){encodeURI
+  prepend(data){
     const node = new Node(data)
     node.nextNode = this.head
     this.head = node
     this.size++
   }
 
-  printData() {
+  printData(){
     let current = this.head;
     while (current) {
       console.log(current.value);
       current = current.nextNode;
     }
   }
+
+  getSize(){
+    // let current = this.head
+    // let count = 1
+    // while(current.nextNode){
+    //     current = current.nextNode 
+    //     count++
+    // }
+    console.log(this.size)
+  }
+
+  getHead(){
+    console.log(this.head)
+  }
+
+  getTail(){
+    let current = this.head
+    while(current.nextNode){
+        current = current.nextNode
+    }
+    console.log(current)
+  }
+
+  getAtIndex(index){
+    if(index < 0 || index > this.size) return console.log('nope!')
+    let current = this.head
+    let count = 0
+    while(count < index){
+        current = current.nextNode
+        count++
+    }
+    console.log(current)
+  }
+
+  pop(){
+    if(!this.head) return null
+    if(!this.head.nextNode)  return this.head = null; 
+    let previous = this.head
+    let current = this.head.nextNode
+    while(current.nextNode){
+        previous = current
+        current = current.nextNode
+    }
+    previous.nextNode = null
+  }
 }
 
 const ll = new LinkedList();
-ll.append(100);
-ll.append(200)
-ll.prepend(300)
-ll.printData();
+ll.append(100)
+// ll.append(200)
+// ll.prepend(300)
+ll.pop()
+ll.printData()
