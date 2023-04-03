e863effd7f4b72930ee3af514e9eb89a2a8bc095
Ivijan <iapetras@hotmail.com>
2023-04-03 17:17:47 +0200

    finished BTS

--- algorithms-data-data-structure/BTS/binarySearchTree.js
+++ algorithms-data-data-structure/BTS/binarySearchTree.js
@@ -1,31 +1,184 @@
-class Node{
-    constructor(data = null, left = null, right = null){
-        this.data = data
-        this.left = left
-        this.right = right
+const Node = (value = null, leftChild = null, rightChild = null) => {
+    return {
+      value,
+      leftChild,
+      rightChild
     }
-}
+  }
 
-class Tree{
-    constructor(arr){
-        let soretdArr = mergeSort(arr)
-        this.root = this.buildTree(soretdArr)
+class BinaryTree {
+    constructor(array) {
+      const sortedArray = mergeSort(array);
+      this.root = this.buildTree(sortedArray);
     }
-
-    buildTree(arr){
-    
-        if(arr.length === 0) return null
-
-        let mid = Math.floor(arr.length / 2)
-        const node = new Node(arr[mid])
-    
-        node.left = this.buildTree(arr.slice(0, mid))
-        node.right = this.buildTree(arr.slice(mid + 1))
-        
-        return node
+  
+    buildTree(sortedArray) {
+      if (sortedArray.length === 0) return null;
+  
+      const midpoint = Math.floor(sortedArray.length / 2);
+      const newNode = Node(sortedArray[midpoint]);
+      newNode.leftChild = this.buildTree(sortedArray.slice(0, midpoint));
+      newNode.rightChild = this.buildTree(sortedArray.slice(midpoint + 1));
+      return newNode;
     }
-    
-}
+  
+    insert(value, currentNode = this.root) {
+      if (currentNode === null) return Node(value);
+      if (currentNode.value === value) return;
+  
+      if (currentNode.value < value) {
+        currentNode.rightChild = this.insert(value, currentNode.rightChild);
+      } else {
+        currentNode.leftChild = this.insert(value, currentNode.leftChild);
+      }
+      return currentNode;
+    }
+  
+    remove(value, currentNode = this.root) {
+      if (currentNode === null) return currentNode;
+  
+      if (currentNode.value === value) {
+        currentNode = this.#removeNodeHelper(currentNode);
+      } else if (currentNode.value > value) {
+        currentNode.leftChild = this.remove(value, currentNode.leftChild);
+      } else {
+        currentNode.rightChild = this.remove(value, currentNode.rightChild);
+      }
+      return currentNode;
+    }
+  
+    find(value, node = this.root) {
+      if (node === null || node.value === value) return node;
+  
+      if (node.value < value) {
+        return this.find(value, node.rightChild);
+      } else {
+        return this.find(value, node.leftChild);
+      }
+    }
+  
+    levelOrder(callbackFn) {
+      const queue = [this.root];
+      const levelOrderList = [];
+      while (queue.length > 0) {
+        const currentNode = queue.shift();
+        callbackFn ? callbackFn(currentNode) : levelOrderList.push(currentNode.value);
+  
+        const enqueueList = [
+          currentNode?.leftChild,
+          currentNode?.rightChild
+        ].filter((value) => value);
+        queue.push(...enqueueList);
+      }
+      if (levelOrderList.length > 0) return levelOrderList;
+    }
+  
+    inorder(callbackFn, node = this.root, inorderList = []) {
+      if (node === null) return;
+  
+      this.inorder(callbackFn, node.leftChild, inorderList);
+      callbackFn ? callbackFn(node) : inorderList.push(node.value);
+      this.inorder(callbackFn, node.rightChild, inorderList);
+  
+      if (inorderList.length > 0) return inorderList;
+    }
+  
+    preorder(callbackFn, node = this.root, preorderList = []) {
+      if (node === null) return;
+  
+      callbackFn ? callbackFn(node) : preorderList.push(node.value);
+      this.preorder(callbackFn, node.leftChild, preorderList);
+      this.preorder(callbackFn, node.rightChild, preorderList);
+  
+      if (preorderList.length > 0) return preorderList;
+    }
+  
+    postorder(callbackFn, node = this.root, postorderList = []) {
+      if (node === null) return;
+  
+      this.postorder(callbackFn, node.leftChild, postorderList,);
+      this.postorder(callbackFn, node.rightChild, postorderList);
+      callbackFn ? callbackFn(node) : postorderList.push(node.value);
+  
+      if (postorderList.length > 0) return postorderList;
+    }
+  
+    height(node = this.root) {
+      if (node === null) return 0;
+  
+      const leftHeight = this.height(node.leftChild);
+      const rightHeight = this.height(node.rightChild);
+  
+      return Math.max(leftHeight, rightHeight) + 1;
+    }
+  
+    depth(nodeVal, node = this.root, edgeCount = 0) {
+      if (node === null) return;
+      if (node.value === nodeVal) return edgeCount;
+  
+      if (node.value < nodeVal) {
+        return this.depth(nodeVal, node.rightChild, edgeCount + 1);
+      } else {
+        return this.depth(nodeVal, node.leftChild, edgeCount + 1);
+      }
+    }
+  
+    isBalanced() {
+      return this.#testBalance(this.root) !== -1;
+    }
+  
+    rebalance() {
+      const inorderList = this.inorder();
+      this.root = this.buildTree(inorderList);
+    }
+  
+    prettyPrint(node = this.root, prefix = "", isLeft = true) {
+      if (node.rightChild) {
+        this.prettyPrint(node.rightChild, `${prefix}${isLeft ? '|   ' : '    '}`, false)
+      }
+      console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.value}`);
+      if (node.leftChild) {
+        this.prettyPrint(node.leftChild, `${prefix}${isLeft ? '    ' : '|   '}`, true)
+      }
+    }
+  
+    // private methods
+    #testBalance(node) {
+      if (node === null) return 0;
+  
+      const leftBalance = this.#testBalance(node.leftChild);
+      const rightBalance = this.#testBalance(node.rightChild);
+      const diff = Math.abs(leftBalance - rightBalance);
+  
+      if (leftBalance === -1 || rightBalance === -1 || diff > 1) {
+        return -1;
+      } else {
+        return Math.max(leftBalance, rightBalance) + 1;
+      }
+    }
+  
+    #inorderSuccessorFor(node) {
+      let currentNode = node;
+      while (currentNode.leftChild) {
+        currentNode = currentNode.leftChild;
+      }
+      return currentNode;
+    }
+  
+    #removeNodeHelper(node) {
+      if (node.leftChild && node.rightChild) {
+        const successorNode = this.#inorderSuccessorFor(node.rightChild);
+        node.value = successorNode.value;
+        node.rightChild = this.remove(successorNode.value, node.rightChild);
+        return node;
+      } else {
+        const replacementNode = node.rightChild || node.leftChild;
+        node = null;
+        return replacementNode;
+      }
+    }
+  }
+
 
 function merge(left,right){
     let sortedArr = []
@@ -48,6 +201,6 @@ function mergeSort(arr) {
     return merge(left, right) 
   }
 
-  let newTree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
+ 
 
-console.log(newTree)
+  console.log(new BinaryTree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]))
