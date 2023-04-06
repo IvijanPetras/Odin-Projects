59e8010bfa31d2ad8a93aff9383cfe021c8893c3
Ivijan <iapetras@hotmail.com>
2023-04-06 19:07:02 +0200

    caesar cipher

--- TDD/script.js
+++ TDD/script.js
@@ -1,32 +1,62 @@
-export function capitalize(string){
- let cap = string.charAt(0).toUpperCase() + string.slice(1)
- return cap
+export function capitalize(string) {
+  let cap = string.charAt(0).toUpperCase() + string.slice(1);
+  return cap;
 }
 
-export function reverseString(string){
-    const stringArr = string.split("")
-    console.log(stringArr)
-    let reversedArr = []
-    for (let i = 0; i < string.length; i++) {
-        reversedArr.push(stringArr.pop())
-    }
-    return reversedArr.join('')
+export function reverseString(string) {
+  const stringArr = string.split("");
+  console.log(stringArr);
+  let reversedArr = [];
+  for (let i = 0; i < string.length; i++) {
+    reversedArr.push(stringArr.pop());
+  }
+  return reversedArr.join("");
 }
 
-export class Calculator{
-    constructor(a,b){
-        this.a = a
-        this.b = b  
-    }
+export class Calculator {
+  constructor(a, b) {
+    this.a = a;
+    this.b = b;
+  }
 
-    add(){
-        return this.a + this.b
-    }
+  add() {
+    return this.a + this.b;
+  }
 
-    substract(){
-        return this.a - this.b
-    }
+  substract() {
+    return this.a - this.b;
+  }
+
+  divide() {
+    let division = this.a / this.b;
+    return +division.toFixed(1);
+  }
+
+  multipy() {
+    return this.a * this.b;
+  }
 }
 
-let calc = new Calculator(2,7)
-console.log(calc.substract())
+export const caesar = function (string, shift) {
+  const codeSet = (code) => (code < 97 ? 65 : 97);
+
+  const mod = (n, m) => ((n % m) + m) % m;
+
+  const shiftChar = (char, shift) => {
+    const code = char.charCodeAt();
+
+    if ((code >= 65 && code <= 90) || (code >= 97 && code <= 122)) {
+      return String.fromCharCode(
+        mod(code + shift - codeSet(code), 26) + codeSet(code)
+      );
+    }
+    return char;
+  };
+
+  return string
+    .split("")
+    .map((char) => shiftChar(char, shift))
+    .join("");
+};
+
+console.log(caesar("defend the east wall of the castle", 1));
--- TDD/script.test.js
+++ TDD/script.test.js
@@ -1,4 +1,5 @@
-import { capitalize, reverseString } from "./script"
+import expect from "expect"
+import { capitalize, reverseString, Calculator, caesar } from "./script"
 
 test('capitalize', () => {
     expect(capitalize('ivijan')).toBe('Ivijan')
@@ -7,4 +8,19 @@ test('capitalize', () => {
 test('reversed string', () => {
     expect(reverseString('Ivijan')).toBe('najivI')
     expect(reverseString('Ivijan aha')).toBe('aha najivI')
+})
+
+let calc = new Calculator(2,7)
+
+test('calc test', () => {
+    expect(calc.add()).toBe(9)
+    expect(calc.substract()).toBe(-5)
+    expect(calc.divide()).toBe(0.3)
+    expect(calc.multipy()).toBe(14)
+})
+
+test('ceaser', () => {
+    expect(caesar('defend the east wall of the castle', 1)).toBe('efgfoe uif fbtu xbmm pg uif dbtumf')
+    expect(caesar('defend the east wall of the castle', 0)).toBe('defend the east wall of the castle')
+    expect(caesar('defend the east wall of the castle.==', 0)).toBe('defend the east wall of the castle.==')
 })
