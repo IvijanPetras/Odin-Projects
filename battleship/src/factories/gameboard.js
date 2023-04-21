d06a99a4b14f8e96677c9cbdfe15373beb11e30a
Ivijan <iapetras@hotmail.com>
2023-04-21 15:20:41 +0200

    working on gameboard and ship placement

--- battleship/src/factories/gameboard.js
+++ battleship/src/factories/gameboard.js
@@ -1,16 +1,26 @@
+const CreateShip = require('./ship')
 const playerBoard = document.querySelector('.player-board')
 const aiBoard = document.querySelector('.ai-board')
+
+const ships = CreateShip.CreateShip()
+
+function cellListener(e){
+    
+}
 
 function Gameboard(size){
     const board = []
     const numOfCells = size * size
     for (let i = 0; i < numOfCells; i++) {
         const cell = document.createElement('div')
-        cell
+        cell.className = 'cell'
         cell.innerText = i
-        const cellId = i
+        cell.setAttribute('id', `${i}`)
+        cell.addEventListener('click', (e) => {
+            cellListener(e)
+        })
         playerBoard.append(cell)
-        board.push(cellId)
+        board.push(i)
     }
     return board
 }
