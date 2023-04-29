e25caa7151b3fee121ec6f577c59f8577ac666c7
Ivijan <iapetras@hotmail.com>
2023-04-29 13:46:52 +0200

    adding listeners

--- battleship/src/helpers/elements.js
+++ battleship/src/helpers/elements.js
@@ -4,18 +4,31 @@ const battleships = document.querySelector(".battleships");
 
 function dragAndDropShip() {}
 
+function clickedPositionListener(e) {
+  const row = e.target.parentNode.dataset.rowNumber;
+  const col = e.target.dataset.colNumber;
+  return {
+    row,
+    col
+  }
+}
+
 function createUIBoard(board) {
-   board.forEach(el => {
-    const cell = document.createElement('div')
-    cell.className = board.indexOf(el)
+  board.forEach((el) => {
+    const row = document.createElement("div");
+    row.className = "row";
+    row.setAttribute("data-row-number", board.indexOf(el));
     for (let i = 0; i < el.length; i++) {
-        const cellChild = document.createElement('div')
-        cellChild.innerText = i
-        cellChild.setAttribute('id', i)
-        cell.append(cellChild)
+      const rowCell = document.createElement("div");
+      rowCell.innerText = i;
+      rowCell.setAttribute("data-col-number", i);
+      rowCell.addEventListener("click", (e) => {
+        clickedPositionListener(e);
+      });
+      row.append(rowCell);
     }
-    playerBoard.append(cell)
-   });
+    playerBoard.append(row);
+  });
 }
 
 module.exports = { createUIBoard };
